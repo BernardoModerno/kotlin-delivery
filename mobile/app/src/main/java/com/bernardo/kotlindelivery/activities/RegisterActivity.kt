951a237da8f7910bd020de1a0650415da1a91aad
BernardoModerno <bernardo.moderno@gmail.com>
2022-04-11 21:33:36 -0300

    Navegar de uma tela a outra

--- mobile/app/src/main/java/com/bernardo/kotlindelivery/activities/RegisterActivity.kt
+++ mobile/app/src/main/java/com/bernardo/kotlindelivery/activities/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.bernardo.kotlindelivery
+package com.bernardo.kotlindelivery.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bernardo.kotlindelivery.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
