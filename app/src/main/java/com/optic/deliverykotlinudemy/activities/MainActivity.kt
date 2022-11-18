9b816dda582785f1d1079246363a8524da2d2143
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-18 18:48:36 -0300

    Obtendo valores dos campos

--- app/src/main/java/com/optic/deliverykotlinudemy/activities/MainActivity.kt
+++ app/src/main/java/com/optic/deliverykotlinudemy/activities/MainActivity.kt
@@ -3,20 +3,42 @@ package com.optic.deliverykotlinudemy.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import com.optic.deliverykotlinudemy.R
 
 class MainActivity : AppCompatActivity() {
 
     var imageViewGoToRegister: ImageView? = null
+    var editTextEmail: EditText? = null
+    var editTextPassword: EditText? = null
+    var buttonLogin: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imageViewGoToRegister = findViewById(R.id.imageview_go_to_register)
+        editTextEmail = findViewById(R.id.edittext_email)
+        editTextPassword = findViewById(R.id.edittext_password)
+        buttonLogin = findViewById(R.id.btn_login)
 
         imageViewGoToRegister?.setOnClickListener { goToRegister() }
+        buttonLogin?.setOnClickListener { login() }
+    }
+
+    private fun login() {
+        val email = editTextEmail?.text.toString() // NULL POINTER EXCEPTION
+        val password = editTextPassword?.text.toString()
+
+        Toast.makeText(this, "O email é: $email", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "O password é: $password", Toast.LENGTH_LONG).show()
+
+        Log.d("MainActivity", "O email é: $email")
+        Log.d("MainActivity", "O password é: $password")
     }
 
     private fun goToRegister() {
--- app/src/main/java/com/optic/deliverykotlinudemy/activities/RegisterActivity.kt
+++ app/src/main/java/com/optic/deliverykotlinudemy/activities/RegisterActivity.kt
@@ -3,20 +3,58 @@ package com.optic.deliverykotlinudemy.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import com.optic.deliverykotlinudemy.R
 
 class RegisterActivity : AppCompatActivity() {
 
+    val TAG = "RegisterActivity"
+
     var imageViewGoToLogin: ImageView? = null
+    var editTextName: EditText? = null
+    var editTextLastname: EditText? = null
+    var editTextEmail: EditText? = null
+    var editTextPhone: EditText? = null
+    var editTextPassword: EditText? = null
+    var editTextConfirmPassword: EditText? = null
+    var buttonRegister: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         imageViewGoToLogin = findViewById(R.id.imageview_go_to_login)
+        editTextName = findViewById(R.id.edittext_name)
+        editTextLastname = findViewById(R.id.edittext_lastname)
+        editTextEmail = findViewById(R.id.edittext_email)
+        editTextPhone = findViewById(R.id.edittext_phone)
+        editTextConfirmPassword = findViewById(R.id.edittext_confirm_password)
+        editTextPassword = findViewById(R.id.edittext_password)
+        buttonRegister = findViewById(R.id.btn_register)
+
 
         imageViewGoToLogin?.setOnClickListener { goToLogin() }
+        buttonRegister?.setOnClickListener { register() }
+    }
+
+    private fun register() {
+        val name = editTextName?.text.toString()
+        val lastname = editTextLastname?.text.toString()
+        val email = editTextEmail?.text.toString()
+        val phone = editTextPhone?.text.toString()
+        val password = editTextPassword?.text.toString()
+        val confirmPassword = editTextConfirmPassword?.text.toString()
+
+        Log.d(TAG, "O nome é: $name")
+        Log.d(TAG, "O sobrenome é: $lastname")
+        Log.d(TAG, "O email é: $email")
+        Log.d(TAG, "O Telefone é: $phone")
+        Log.d(TAG, "O password é: $password")
+        Log.d(TAG, "O confirm password é: $confirmPassword")
+
     }
 
     private fun goToLogin() {
