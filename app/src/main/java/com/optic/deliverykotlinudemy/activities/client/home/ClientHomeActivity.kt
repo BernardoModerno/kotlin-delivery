9ed42fd17111aac82835fd8c4398d15ea34b2db4
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-28 16:20:33 -0300

    Mostrar dados usuário em fragment client

--- app/src/main/java/com/optic/deliverykotlinudemy/activities/client/home/ClientHomeActivity.kt
+++ app/src/main/java/com/optic/deliverykotlinudemy/activities/client/home/ClientHomeActivity.kt
@@ -69,11 +69,7 @@ class ClientHomeActivity : AppCompatActivity() {
         transaction.commit()
     }
 
-    private fun logout() {
-        sharedPref?.remove("user")
-        val i = Intent(this, MainActivity::class.java)
-        startActivity(i)
-    }
+
 
     private fun getUserFromSession() {
 
